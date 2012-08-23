7832257a33f8c855dd98fce3b725fbb61409439b
N. Heinrichs <nheinric@cpan.org>
2012-08-23 21:18:41 +0900

    Only downgrade TLS when using iOS < 5.1

--- Classes/ASIHTTPRequest.m
+++ Classes/ASIHTTPRequest.m
@@ -1207,12 +1207,15 @@ static NSOperationQueue *sharedQueue = nil;
 
     if([[[[self url] scheme] lowercaseString] isEqualToString:@"https"]) {
         
-        // see: https://github.com/ignaval/asi-http-request/commit/c782abbeb204156d30ecbb902915d1eaf9b10f9e
-        NSMutableDictionary *sslProperties = [NSMutableDictionary dictionaryWithCapacity:1];
-        [sslProperties setValue:@"kCFStreamSocketSecurityLevelTLSv1_0SSLv3" forKey:kCFStreamSSLLevel];
-        CFReadStreamSetProperty((CFReadStreamRef)[self readStream],
-                                kCFStreamPropertySSLSettings,
-                                (CFTypeRef)sslProperties);
+        float sysVer = [[[UIDevice currentDevice] systemVersion] floatValue];
+        if ( sysVer < 5.1f ) {
+            // see: https://github.com/ignaval/asi-http-request/commit/c782abbeb204156d30ecbb902915d1eaf9b10f9e
+            NSMutableDictionary *sslProperties = [NSMutableDictionary dictionaryWithCapacity:1];
+            [sslProperties setValue:@"kCFStreamSocketSecurityLevelTLSv1_0SSLv3" forKey:kCFStreamSSLLevel];
+            CFReadStreamSetProperty((CFReadStreamRef)[self readStream],
+                                    kCFStreamPropertySSLSettings,
+                                    (CFTypeRef)sslProperties);
+        }
 
         //
         // Tell CFNetwork not to validate SSL certificates
